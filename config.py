e799f87660c7cee612833a4107e9acd0c7ace94a
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2022-03-22 08:14:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = ''  # чтобы другие боты не парсили токены с гита,
+# вставьте отсюда: 5169419362(_ДВОЕТОЧИЕ_)AAGrMUByoEbYsYkL4CmM7hGNtJ9E3CRE8t0
+WEATHER_TOKEN = ''  # 96be59a80b28ccefe2dca44f865b5c71
+
+currency = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
